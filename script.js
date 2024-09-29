7c8678d4d2977310737881eb72780da0fec0af09
Blanzy124 <samuelmedinablandon124@gmail.com>
2024-09-29 11:46:37 -0700

    1.12

---
+++ script.js
@@ -0,0 +1,10 @@
+// Realizamos una operación en JavaScript
+let a = 10;
+let b = 20;
+let suma = a + b;
+
+// Seleccionamos el elemento donde queremos mostrar el resultado
+let elementoResultado = document.getElementById("resultado");
+
+// Cambiamos el contenido de texto del elemento
+elementoResultado.textContent = "El resultado de la suma es: " + suma;
