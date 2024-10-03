e01f64acac277ff0b51ad4c282d289c619b62add
Blanzy124 <samuelmedinablandon124@gmail.com>
2024-10-02 23:17:14 -0700

    1,0,0

--- script.js
+++ script.js
@@ -3,22 +3,14 @@ function suma(){
  var sn = document.getElementById('sn').value;
  localStorage.setItem('rsu', parseFloat(pn) + parseFloat(sn));
  
- var st1 = document.getElementById('suma').value;
- if (st1 === true){
-  var st = 1;
-  var rt = 0;
-  var mt = 0;
-  var dt = 0;
-
-  localStorage.setItem('st', st);
-  localStorage.setItem('rt', rt);
-  localStorage.setItem('mt', mt);
-  localStorage.setItem('dt', dt);
+  localStorage.setItem('st', 1);
+  localStorage.setItem('rt', 0);
+  localStorage.setItem('mt', 0);
+  localStorage.setItem('dt', 0);
 
   localStorage.setItem('rre', '0')
   localStorage.setItem('rmu', '0')
   localStorage.setItem('rdi', '0')
- }
 }
 
 
@@ -26,66 +18,102 @@ function resta(){
  var pn = document.getElementById('pn').value;
  var sn = document.getElementById('sn').value;
  localStorage.setItem('rre', parseFloat(pn) - parseFloat(sn));
+ 
+  localStorage.setItem('st', 0);
+  localStorage.setItem('rt', 1);
+  localStorage.setItem('mt', 0);
+  localStorage.setItem('dt', 0);
 
- var rre = parseFloat(pn) + parseFloat(sn);
+  localStorage.setItem('rsu', '0')
+  localStorage.setItem('rmu', '0')
+  localStorage.setItem('rdi', '0')
+ 
 }
 
 
 function multiplicacion(){
  var pn = document.getElementById('pn').value;
  var sn = document.getElementById('sn').value;
- var rmu = parseFloat(pn) + parseFloat(sn);
+ localStorage.setItem('rmu', parseFloat(pn) * parseFloat(sn));
+ 
+ localStorage.setItem('st', 0);
+ localStorage.setItem('rt', 0);
+ localStorage.setItem('mt', 1);
+ localStorage.setItem('dt', 0);
+ 
+ localStorage.setItem('rsu', '0')
+ localStorage.setItem('rre', '0')
+ localStorage.setItem('rdi', '0')
 }
 
 
 function divicion(){
  var pn = document.getElementById('pn').value;
  var sn = document.getElementById('sn').value;
- var rdi = parseFloat(pn) + parseFloat(sn);
+ localStorage.setItem('rdi', parseFloat(pn) / parseFloat(sn));
+ 
+ localStorage.setItem('st', 0);
+ localStorage.setItem('rt', 0);
+ localStorage.setItem('mt', 0);
+ localStorage.setItem('dt', 1);
+
+ localStorage.setItem('rsu', '0')
+ localStorage.setItem('rre', '0')
+ localStorage.setItem('rmu', '0')
 }
 
-
+///////////////////////////////////////
 function mostrar(){
- var st = localStorage.getItem('st');
- var rt = localStorage.getItem('rt');
- var mt = localStorage.getItem('mt');
- var dt = localStorage.getItem('dt');
+ var st = parseFloat(localStorage.getItem('st'));
+ console.log(st);
+ var rt = parseFloat(localStorage.getItem('rt'));
+ console.log(rt);
+ var mt = parseFloat(localStorage.getItem('mt'));
+ console.log(mt);
+ var dt = parseFloat(localStorage.getItem('dt'));
+ console.log(dt);
 
- var rsu = localStorage.getItem('rsu');
- var rre = localStorage.getItem('rre');
- var rmu = localStorage.getItem('rmu');
- var rdi = localStorage.getItem('rdi');
+ console.log("rrr");
 
+ var rsu = parseFloat(localStorage.getItem('rsu'));
+ console.log(rsu);
+ var rre = parseFloat(localStorage.getItem('rre'));
+ console.log(rre);
+ var rmu = parseFloat(localStorage.getItem('rmu'));
+ console.log(rmu);
+ var rdi = parseFloat(localStorage.getItem('rdi'));
+ console.log(rdi);
 
+console.log("ifs")
 
-
-
- if (st = 1){
+switch (st){
+ case 1: 
  document.getElementById('resultado').textContent = rsu;
+ console.log('1if ' + rsu);
  localStorage.clear();
- return;
-
-
+ case 0:;
 }
- if (rt = 1){
- document.getElementById('resultado').textContent = rre;
+switch (rt){
+ case 1:
+  document.getElementById('resultado').textContent = rre;
+ console.log('2if ' + rre);
  localStorage.clear();
- return;
-
-
+ case 0:;
 }
- if (mt = 1){
- document.getElementById('resultado').textContent = rmu;
+switch (mt){
+ case 1:
+  document.getElementById('resultado').textContent = rmu;
+ console.log('3if ' + rmu);
  localStorage.clear();
- return;
-
-
+ case 0:;
 }
- if (dt = 1){
+switch (dt){
+ case 1: 
  document.getElementById('resultado').textContent = rdi;
+ console.log('4if ' + rdi);
  localStorage.clear();
- return;
-
-
- }
+ case 0:;
 }
+}
+
+
