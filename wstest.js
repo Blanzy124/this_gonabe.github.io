95db9692cd9d2d9eeaec9f3a672f6e3029e7bd4d
Blanzy124 <samuelmedinablandon124@gmail.com>
2025-05-31 14:20:03 -0700

    lalala

--- wstest.js
+++ wstest.js
@@ -12,7 +12,7 @@ const socket = new WebSocket(`wss://localhost:8443/foo?token=${await JWT()}`);
 
 socket.addEventListener("open", async (event) => {
   const message = `${userNameLV} is connected`
-  const data = { message: message, to: messageTo || "nobody"};
+  const data = { message: message, from: userNameLV , to: messageTo || "nobody"};
   console.log('Connected')
   socket.send(JSON.stringify(data));
 });
@@ -26,7 +26,7 @@ socket.addEventListener("message", async (event) => {
 document.getElementById("send").addEventListener("click", function () {
  const message = document.getElementById("messageIn").value;
  const messageTo = document.getElementById("messageTo").value;
-  const data = { message: message, to: messageTo || "nobody"}
+  const data = { message: message, from: userNameLV , to: messageTo || "nobody"}
  socket.send(JSON.stringify(data));
  document.getElementById("formIn").reset();
 
